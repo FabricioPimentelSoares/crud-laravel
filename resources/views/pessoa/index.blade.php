bf1a2d50a35aefa165b7b1f385b6379b4a2b9ca1
Fabricio Pimentel <fabriciosoares.br@gmail.com>
2020-08-15 15:24:56 -0400

    Tabela Atualizada

--- resources/views/pessoa/index.blade.php
+++ resources/views/pessoa/index.blade.php
@@ -1,13 +1,24 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <h1>Pessoas cadastradas</h1>
 
-<a href="/pessoas/create">Cadastrar Nova</a>
+<a class="btn btn-primary" href="/pessoas/create">Novo Cadastro</a>
 
 <hr>
 
-@foreach($pessoas as $p)
-    <p>Nome: {{ $p->nome}}</p>
-    <p>Telefone: {{ $p->telefone}}</p>
-    <p>Email: {{ $p->email}}</p>
+<table class="table table-bordered table-dark">
+    <tr>
+        <th>Nome</th>
+        <th>Telefone</th>
+        <th>Email</th>
+        <th>Ações</th>
+    </tr>
+    @foreach($pessoas as $p)
+    <tr>
+        <td>{{ $p->nome}}</td>
+        <td>{{ $p->telefone}}</td>
+        <td>{{ $p->email}}</td>
+        <td>---</td>
+    </tr>
+    @endforeach
 
-    <hr>
-@endforeach
+</table>
